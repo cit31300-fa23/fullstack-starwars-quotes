f9eddc2006b5ce976dc0aa33939b4d49dd4471f2
Rob Elliott <elliott@iu.edu>
2023-11-15 14:35:32 -0500

    Added client with useEffect hooks to API

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 import Character from "src/components/Character/Character";
+import axios from 'axios';
+import { nanoid } from 'nanoid';
 
 function App(props) {
-  const [characters, setCharacters] = useState(props.characters)
+  const [characters, setCharacters] = useState([]);
+
+  // Add hook to load a list of characters when the component renders
+  useEffect(() => {
+    // Define a function that loads characters from the API
+    const loadCharacters = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8080/api/v1/characters`)
+        console.log(response.data);
+        setCharacters(() => [...response.data]);
+      }
+      catch (error) {
+        console.log('Error!');
+      }
+    };
+
+    // Call the function to actually connect to the API and load data
+    loadCharacters();
+
+  }, []);
 
   // Create an instance of the Character component for each element in the characters array
-  const characterList = characters.map((char) => <Character name={ char.name } quotes={ char.quotes } />)
+  const characterList = characters.map((char) => <Character 
+    name={ char.name } 
+    id={ `char-${nanoid()}` }
+    key={ `char-${nanoid()}` }
+    //quotes={ char.quotes } 
+  />)
   
   return (
     <div className="App">
-      <div class="container">
+      <div className="container">
         { characterList }
       </div>
     </div>
--- client/src/components/Character/Character.js
+++ client/src/components/Character/Character.js
@@ -1,10 +1,34 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Quote from "src/components/Quote/Quote";
+import axios from 'axios';
+import { nanoid } from 'nanoid';
 
 function Character(props) {
-    const [quotes, setQuotes] = useState(props.quotes)
+    const [quotes, setQuotes] = useState([])
 
-    const quotesList = quotes.map((quote) => <Quote quoteText={ quote } />)
+    // Add hook to contact API and retrieve quotes for this character
+    useEffect(() => {
+        // Define a function that contacts the API and retrieves data
+        const loadQuotes = async () => {
+            try {
+                const response = await axios.get(`http://localhost:8080/api/v1/quotes/character/${props.name}`)
+                setQuotes(() => [...response.data]);
+            }
+            catch (error) {
+                console.log('Error!');
+            }
+            
+        };
+
+        // Call the function
+        loadQuotes();
+    }, []);
+
+    const quotesList = quotes.map((quote) => <Quote 
+    quoteText={ quote } 
+    id={ `quote-${nanoid()}` }
+    key={ `quote-${nanoid()}` }
+    />)
 
     return (        
         <div class="row border rounded p-4 m-2">
